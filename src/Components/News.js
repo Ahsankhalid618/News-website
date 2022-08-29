77062b01c93c019cd63dd857b5a4978ba19f70d5
Ahsan khalid <ahsankhalid816@gmail.com>
2022-08-30 02:43:04 +0500

    function based work completed

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,22 +1,19 @@
-import React, { useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = (props)=>{ 
+const News = (props) => {
   // document.title = `${capitalizeFirstLetter(props.category)} | NewsApp`;
- const capitalizeFirstLetter = (string) => {
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  
-const [articles, setArticles] = useState([])
-const [page, setPage] = useState(1)
-const [totalResults, setTotalResults] = useState(0)
+  const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-
-  
-const newsUpdate = async () => {
+  const newsUpdate = async () => {
     props.setProgress(0);
     const Url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
     let data = await fetch(Url);
@@ -25,87 +22,81 @@ const newsUpdate = async () => {
     props.setProgress(70);
     console.log(parseData);
     setArticles(parseData.articles);
-    setTotalResults(parseData.totalResults)
+    setTotalResults(parseData.totalResults);
 
     props.setProgress(100);
   };
 
   useEffect(() => {
     newsUpdate();
-  })
-  
+  });
 
- const fetchMoreData = async () => {
-  setPage(page+1)
-    
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+
     const Url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=436022d4a71f42d09cf2c2d0f1762b8b&page=${page}&pageSize=${props.pageSize}`;
     let data = await fetch(Url);
     let parseData = await data.json();
     console.log(parseData);
-    setArticles(articles.concat(parseData.articles))
-    setTotalResults( parseData.totalResults)
+    setArticles(articles.concat(parseData.articles));
+    setTotalResults(parseData.totalResults);
   };
-//  const nextbtn = async () => {
-//     setPage(page + 1);
-//     newsUpdate();
-//   };
-//  const prevbtn = async () => {
-//     setPage(page - 1);
-//     newsUpdate();
-//   };
-  
-    return (
-      <div className="container">
-        <h1 className="text-center mb-5 mt-5">
-          News App | Top {capitalizeFirstLetter(props.category)}{" "}
-          Headlines
-        </h1>
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          loader={
-            !totalResults && (
-              <h4 className="text-center">Loading...</h4>
-            )
-          }
-        >
-          <div className="container">
-            <div
-              className="row"
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "flex-start",
-              }}
-            >
-              {articles?.map((element) => {
-                return (
-                  <div className="col-md-3 my-2 mb" key={element.url}>
-                    <NewsItem
-                      title={element.title ? element.title.slice(0, 45) : ""}
-                      description={
-                        element.description
-                          ? element.description.slice(0, 62)
-                          : ""
-                      }
-                      urlToImage={element.urlToImage}
-                      newsUrl={element.url}
-                      date={element.publishedAt}
-                      author={element.author ? element.author : "Unknown"}
-                      source={
-                        element.source.name ? element.source.name : "Unknown"
-                      }
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </InfiniteScroll>
+  //  const nextbtn = async () => {
+  //     setPage(page + 1);
+  //     newsUpdate();
+  //   };
+  //  const prevbtn = async () => {
+  //     setPage(page - 1);
+  //     newsUpdate();
+  //   };
 
-        <div className="d-flex justify-content-between">
-          {/* <button
+  return (
+    <div className="container">
+      <h1 className="text-center mb-5 mt-5">
+        News App | Top {capitalizeFirstLetter(props.category)} Headlines
+      </h1>
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={!totalResults && <h4 className="text-center">Loading...</h4>}
+      >
+        <div className="container">
+          <div
+            className="row"
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "flex-start",
+            }}
+          >
+            {articles?.map((element) => {
+              return (
+                <div className="col-md-3 my-2 mb" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 62)
+                        : ""
+                    }
+                    urlToImage={element.urlToImage}
+                    newsUrl={element.url}
+                    date={element.publishedAt}
+                    author={element.author ? element.author : "Unknown"}
+                    source={
+                      element.source.name ? element.source.name : "Unknown"
+                    }
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </InfiniteScroll>
+
+      <div className="d-flex justify-content-between">
+        {/* <button
             disabled={this.state.page <= 1}
             type="button"
             onClick={this.prevbtn}
@@ -123,23 +114,21 @@ const newsUpdate = async () => {
           >
             Next &rarr;
           </button> */}
-        </div>
       </div>
-    );
-  
-}
+    </div>
+  );
+};
 
 News.defaultProps = {
-    country: "us",
-    category: "general",
-    key: "general",
-  };
-
+  country: "us",
+  category: "general",
+  key: "general",
+};
 
 News.propTypes = {
-    country: PropTypes.string,
-    category: PropTypes.string,
-    key: PropTypes.string,
-  };
+  country: PropTypes.string,
+  category: PropTypes.string,
+  key: PropTypes.string,
+};
 
 export default News;
